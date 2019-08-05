504e1b97dac96664632a3d28791ecb1533bf69f9
Naoto00 <naoto-nakazato@cybozu.co.jp>
2019-08-05 15:20:47 +0900

    Use form elemet

--- src/js/desktop.js
+++ src/js/desktop.js
@@ -5,27 +5,27 @@
  * Licensed under the MIT License
  */
 (function(PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    // Get plug-in configuration settings
-    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-    // Get each settings
-    if (!CONFIG) {
-        return false;
+  // Get plug-in configuration settings
+  var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+  // Get each settings
+  if (!CONFIG) {
+    return false;
+  }
+
+  var user_selection = CONFIG.user_selection; //Set the field code of the User Selection field
+  var error_message = CONFIG.error_message;
+
+  var myEvent = ['app.record.create.submit', 'app.record.edit.submit', 'app.record.index.edit.submit'];
+  kintone.events.on(myEvent, function(event) {
+    // Get the User Selection field information
+    var record = event.record;
+    var selectedUsers = record[user_selection].value;
+    if (selectedUsers.length > 1) {
+      // If more than one person is specified, set message to error property
+      event.error = error_message;
     }
-
-    var user_selection = CONFIG.user_selection; //Set the field code of the User Selection field
-    var error_message = CONFIG.error_message;
-
-    var myEvent = ['app.record.create.submit', 'app.record.edit.submit', 'app.record.index.edit.submit'];
-    kintone.events.on(myEvent, function(event) {
-        // Get the User Selection field information
-        var record = event.record;
-        var selectedUsers = record[user_selection].value;
-        if (selectedUsers.length > 1) {
-            // If more than one person is specified, set message to error property
-            event.error = error_message;
-        }
-        return event;
-    });
+    return event;
+  });
 })(kintone.$PLUGIN_ID);
